b9cad86fd25039c2d39f394458809edc7bc99f45
Antonio Victor <vischramm@gmail.com>
2025-01-03 06:09:34 -0300

    Finalização

--- app.js
+++ app.js
@@ -18,8 +18,22 @@ app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve(), "views"));
 
 // Rota principal
-app.get("/", (req, res) => {
-    res.render("index"); // Renderiza o arquivo views/index.ejs
+app.get("/", async (req, res) => {
+    try {
+        const drinks = [];
+
+        for (let i = 0; i < 4; i++) {
+            const randomDrink = await axios.get(
+                "https://thecocktaildb.com/api/json/v1/1/random.php"
+            );
+            drinks.push(randomDrink.data);
+        }
+        console.log(drinks[0]);
+        res.render("index", { drinks }); // Renderiza o arquivo views/index.ejs
+    } catch (error) {
+        console.error("Erro ao fazer a chamada:", error.message);
+        res.status(500).json({ error: "Erro ao buscar dados" });
+    }
 });
 
 // Iniciar o servidor
